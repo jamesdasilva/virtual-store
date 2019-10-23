a70706b859e29819a790702cbb880bbed539fb49
James da Silva <jamesodas@gmail.com>
2019-10-23 10:58:39 -0300

    Add tests to QuantityField component

--- src/cart-module/QuantityField/QuantityField.js
+++ src/cart-module/QuantityField/QuantityField.js
@@ -8,9 +8,25 @@ class QuantityField extends React.Component {
     const { value, incrementAmount, decreaseAmount, onChange } = this.props
     return (
       <div className='quantity-field'>
-        <button className='quantity-field__btn-decrement' onClick={decreaseAmount}><div>-</div></button>
-        <input className='quantity-field__value' type='text' value={value} onChange={onChange} />
-        <button className='quantity-field__btn-increment' onClick={incrementAmount}><div>+</div></button>
+        <button
+          className='quantity-field__btn-decrement'
+          data-testid='decrease-button'
+          onClick={decreaseAmount}>
+          <div>-</div>
+        </button>
+
+        <input
+          className='quantity-field__value'
+          type='text'
+          value={value}
+          onChange={onChange} />
+
+        <button
+          className='quantity-field__btn-increment'
+          data-testid='increment-button'
+          onClick={incrementAmount}>
+          <div>+</div>
+        </button>
       </div>
     )
   }
---
+++ src/cart-module/QuantityField/QuantityField.test.js
@@ -0,0 +1,74 @@
+import '@testing-library/jest-dom'
+import { render, cleanup, waitForElement, fireEvent } from '@testing-library/react'
+import React from 'react'
+import QuantityField from './QuantityField'
+
+let incrementAmountSpy
+let decreaseAmountSpy
+let onChangeSpy
+let valueMock
+
+afterEach(cleanup)
+
+beforeEach(() => {
+  incrementAmountSpy = jest.fn()
+  decreaseAmountSpy = jest.fn()
+  onChangeSpy = jest.fn()
+  valueMock = 2
+})
+
+describe('<QuantityField /> spec', () => {
+  test('Should display value', async () => {
+    const { getByDisplayValue } = render(
+      <QuantityField
+        value={valueMock}
+        incrementAmount={incrementAmountSpy}
+        decreaseAmount={decreaseAmountSpy}
+        onChange={onChangeSpy} />
+      )
+    const quantityFieldValue = await waitForElement(() => getByDisplayValue(`${valueMock}`))
+    expect(quantityFieldValue).toBeDefined()
+  })
+
+  test('Should call onChange function when user change value', async () => {
+    const { getByDisplayValue } = render(
+      <QuantityField
+        value={valueMock}
+        incrementAmount={incrementAmountSpy}
+        decreaseAmount={decreaseAmountSpy}
+        onChange={onChangeSpy} />
+      )
+    const quantityFieldValue = await waitForElement(() => getByDisplayValue(`${valueMock}`))
+    fireEvent.change(quantityFieldValue, {
+      target: { value: valueMock + 1 }
+    })
+    expect(onChangeSpy).toBeCalled()
+  })
+
+  test('Should call the increment function when the user clicks the increment button', async () => {
+    const { getByTestId } = render(
+      <QuantityField
+        value={valueMock}
+        incrementAmount={incrementAmountSpy}
+        decreaseAmount={decreaseAmountSpy}
+        onChange={onChangeSpy} />
+      )
+    const incrementButton = await waitForElement(() => getByTestId('increment-button'))
+    fireEvent.click(incrementButton)
+    expect(incrementAmountSpy).toBeCalled()
+  })
+
+  test('Should call the decreaseAmount function when the user clicks the decrease button', async () => {
+    const { getByTestId } = render(
+      <QuantityField
+        value={valueMock}
+        incrementAmount={incrementAmountSpy}
+        decreaseAmount={decreaseAmountSpy}
+        onChange={onChangeSpy} />
+      )
+    const decreaseButton = await waitForElement(() => getByTestId('decrease-button'))
+    fireEvent.click(decreaseButton)
+    expect(decreaseAmountSpy).toBeCalled()
+  })
+
+})
